831ac8621b98616aef91e39523534306d9d0819e
繁源 <fanyuan@fanyuandeMac-mini.local>
2016-02-16 15:42:13 +0800

    initial commit

---
+++ ugoudemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ugoudemo
+//
+//  Created by 繁源 on 16/2/16.
+//  Copyright © 2016年 fanyuan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ugoudemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ugoudemo
+//
+//  Created by 繁源 on 16/2/16.
+//  Copyright © 2016年 fanyuan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
